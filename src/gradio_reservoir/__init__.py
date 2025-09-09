58c8522530f4d166ecdfbb721769c627a237beea
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2025-09-09 01:57:10 +0000

    Update placeholder

--- src/gradio_reservoir/__init__.py
+++ src/gradio_reservoir/__init__.py
@@ -1,25 +1,27 @@
 import gradio as gr
 
 
-def greet(name, age):
+def greet(agent1, agent2, name, age):
     return f"Hello {name}! You are {age} years old."
 
 
 COUNTRIES_PLACEHOLDER = "country\nNew Zealand\nVietnam\nAustralia"
-POLICIES_PLACEHOLDER = (
-    "policy\nBagels can be eaten for lunch\nChicken salt is allowed on fries"
-)
+POLICIES_PLACEHOLDER = "policy\nBagels can be eaten for dinner\nChicken salt is allowed on fries\nKangaroos are not to be ridden"
 
 
 def main():
     demo = gr.Interface(
         fn=greet,
         inputs=[
+            gr.Dropdown(["GPT 4.1", "GPT 4o-mini", "Sonar Pro"], label="Search Agent"),
+            gr.Dropdown(
+                ["GPT 4.1", "GPT 4o-mini", "Sonar Pro"], label="Summariser Agent"
+            ),
             gr.Textbox(
-                label="countries.csv", lines=20, placeholder=COUNTRIES_PLACEHOLDER
+                label="countries.csv", lines=5, placeholder=COUNTRIES_PLACEHOLDER
             ),
             gr.TextArea(
-                label="policies.csv", lines=20, placeholder=POLICIES_PLACEHOLDER
+                label="policies.csv", lines=5, placeholder=POLICIES_PLACEHOLDER
             ),
         ],
         outputs=gr.Textbox(label="output.csv", lines=40),
