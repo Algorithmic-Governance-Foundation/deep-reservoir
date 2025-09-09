0d29cee166a4f6755c6440e3249110ea79e343f1
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2025-09-09 01:30:10 +0000

    Add basic UI

--- src/gradio_reservoir/__init__.py
+++ src/gradio_reservoir/__init__.py
@@ -1,12 +1,19 @@
 import gradio as gr
 
 
-def greet(name):
-    return f"Hello {name}!"
+def greet(name, age):
+    return f"Hello {name}! You are {age} years old."
 
 
 def main():
-    demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    demo = gr.Interface(
+        fn=greet,
+        inputs=[
+            gr.Textbox(label="countries.csv", lines=20, placeholder="country"),
+            gr.TextArea(label="policies.csv", lines=20, placeholder="policy"),
+        ],
+        outputs=gr.Textbox(label="output.csv", lines=40),
+    )
     demo.launch()
 
 
