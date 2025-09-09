7f34210bb55e1e9ccced081ef26a7e16bdc941cc
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2025-09-09 01:35:21 +0000

    Add placeholders

--- src/gradio_reservoir/__init__.py
+++ src/gradio_reservoir/__init__.py
@@ -5,12 +5,22 @@ def greet(name, age):
     return f"Hello {name}! You are {age} years old."
 
 
+COUNTRIES_PLACEHOLDER = "country\nNew Zealand\nVietnam\nAustralia"
+POLICIES_PLACEHOLDER = (
+    "policy\nBagels can be eaten for lunch\nChicken salt is allowed on fries"
+)
+
+
 def main():
     demo = gr.Interface(
         fn=greet,
         inputs=[
-            gr.Textbox(label="countries.csv", lines=20, placeholder="country"),
-            gr.TextArea(label="policies.csv", lines=20, placeholder="policy"),
+            gr.Textbox(
+                label="countries.csv", lines=20, placeholder=COUNTRIES_PLACEHOLDER
+            ),
+            gr.TextArea(
+                label="policies.csv", lines=20, placeholder=POLICIES_PLACEHOLDER
+            ),
         ],
         outputs=gr.Textbox(label="output.csv", lines=40),
     )
